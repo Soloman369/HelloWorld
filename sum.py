d9be6e824eef8e8d9893126f17cc2f635c3b8930
Soloman369 <34165712+Soloman369@users.noreply.github.com>
2019-08-24 10:53:12 +0100

    Add files via upload

--- sum.py
+++ sum.py
@@ -1,7 +1,24 @@
 #Hello_Everybody :)
 
-a = float(input("give me a number"))
+"""a = float(input("give me a number"))
 b = float(input("give me another number"))
 
 c = a + b
-print(c)
+print(c)"""
+
+
+
+c = str(input("which operation you want to do"))
+
+def cal(a,b):
+	if c == "+":
+		return a + b;
+	elif c == "-":
+		return a - b
+	elif c == "*":
+		return a*b
+	elif c == "/":
+		return a/b
+	else:
+		print("operator not found")
+

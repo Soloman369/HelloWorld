569023aaa82cd46806151e77c55b2d3f1c07c932
Soloman369 <34165712+Soloman369@users.noreply.github.com>
2019-08-24 19:57:56 +0100

    Add files via upload

---
+++ factoriel.py
@@ -0,0 +1,19 @@
+import argparse
+
+"""parser = argparse.ArgumentParser()
+parser.add_argument("a", type=int)
+args = parser.parse_args()"""
+
+a = int(input("donnez une valeur pour a"))
+
+
+
+def fact(a):
+	if a == 1:
+		return 1
+	elif a > 1:
+		return fact(a-1)*a
+	else:
+		print("sorry souleiman")
+
+print(fact(a))

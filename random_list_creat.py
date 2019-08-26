3843980a4849227ea27f8c412a72cb4965ea36a4
Soloman369 <34165712+Soloman369@users.noreply.github.com>
2019-08-26 16:26:54 +0100

    Add files via upload

--- random_list_creat.py
+++ random_list_creat.py
@@ -1,4 +1,4 @@
-import random
+"""import random
 
 def generate_list(n_elements, max_possible_value):
   l = []
@@ -19,4 +19,36 @@ for i in range(N):
   p.remove(min(p))
 print("Q = ",Q)
 print('-----')
-print(len(Q))
+print(len(Q))"""
+
+import random
+import time
+
+start = time.time()
+
+def generate_list(n_elements, max_possible_value):
+  l = []
+  for i in range(n_elements):
+    s = random.randint(0, max_possible_value)
+    l.append(s)
+  return l
+
+l = sorted(generate_list(3, 50))
+k = sorted(generate_list(2, 100))
+print('l : ',l)
+print('k : ',k)
+
+def merge_sorted_lists(l,k):
+    p = l + k
+    N = len(p)
+    Q = []
+    for i in range(N):
+        Q.append(min(p))
+        p.remove(min(p))
+    return Q
+
+print("Q : " , merge_sorted_lists(l,k))
+
+end = time.time()
+duration = end - start
+print("Durée de l'opération : " , duration)

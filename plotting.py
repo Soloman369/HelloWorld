1dcfe6ea18f203b9bb0b41944ce431f61f501723
Soloman369 <34165712+Soloman369@users.noreply.github.com>
2019-08-27 23:17:06 +0100

    Add files via upload

--- plotting.py
+++ plotting.py
@@ -1,14 +1,11 @@
 import math
-
 import matplotlib.pyplot as plt
 
 
-x = []
-y = []
 def f(n):
 	total = 0
-	#x = []
-	#y = []
+	x = []
+	y = []
 	for i in range(1, n+1):
 		x.append(i)
 		total += 1/(i*i)
@@ -16,10 +13,58 @@ def f(n):
 	return x, total, y
 
 
-res = f(1000) 
-print(res)
+x, total, y = f(100) 
 
 
+def f(n):
+	a = []
+	b = []
+	total = 0
+	for i in range(1, n+1):
+		a.append(i)
+		total += 1/(i*i*i)
+		b.append(total)
+	return a, total, b
 
-plt.plot(x,y)
+a, total, b = f(100) 
+
+
+def f(n):
+	c = []
+	d = []
+	total = 0
+	for i in range(1, n+1):
+		c.append(i)
+		total += ((-1)**i)/i 
+		d.append(total)
+	return c, total, d 
+
+c, total, d = f(100) 
+
+
+def f(n):
+	e = []
+	g = []
+	total = 0
+	for i in range(0,n):
+		e.append(i)
+		total += 1/math.factorial(i)
+		g.append(total)
+	return e, total, g 
+
+e, total,g = f(100)
+
+#plt.plot(a,b,label='la courbe de 1/i^2')
+
+plt.title('Matplotlib Legend Example')
+
+#plt.plot(x,y,label='la courbe de 1/i^3')
+
+plt.plot(e,g,label='la courbe de 1/i!')
+
+#plt.plot(c,d,label='la courbe de ((-1)**i)/i')
 plt.show()
+
+
+
+
